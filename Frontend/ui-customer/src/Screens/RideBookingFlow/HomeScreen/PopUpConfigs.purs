8b55ef93a7c6c80b94567192d74741d7491632fa
Pravinkumar S <spravinkumar9952@gmail.com>
2024-07-16 15:18:30 +0530

    frontend-feat: NY-RS intercity bus

--- Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/PopUpConfigs.purs
+++ Frontend/ui-customer/src/Screens/RideBookingFlow/HomeScreen/PopUpConfigs.purs
@@ -43,3 +43,35 @@ finalFareExcludesToll lazyCheck = PopUpModal.config{
   , margin = Margin 0 24 0 0
   }
 }
+
+intercityBusPhoneNumberPermission :: HomeScreenState -> PopUpModal.Config
+intercityBusPhoneNumberPermission state = PopUpModal.config{
+  margin = MarginHorizontal 24 24
+, padding =  Padding 16 20 16 20
+, gravity = CENTER
+, buttonLayoutMargin = Margin 0 0 0 0
+, cornerRadius = Corners 24.0 true true true true
+, coverImageConfig{
+    imageUrl = fetchImage APP_ASSET "ny_ic_ny_rs_phone_permission"
+  , visibility = VISIBLE
+  , height =  V 83
+  , width = V $ (screenWidth unit) - 80
+  }
+, primaryText {
+    text = "Phone Number Permission" --getString PHONE_NUMBER_PERMISSION 
+  , color = Color.black800
+  , margin = Margin 0 24 0 0
+  }
+, secondaryText {
+    text = "Redbus wants to know your Namma Yatri phone number" --getString PHONE_NUMBER_PERMISSION_DESC 
+  , margin = Margin 0 8 0 0
+  }
+, option1{
+    text = "Deny" --getString DENY
+    , margin = Margin 0 24 0 0
+  }
+, option2 {
+    text = "Allow" --getString ALLOW
+  , margin = Margin 0 24 0 0
+  }
+}
