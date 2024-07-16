bb4d4ce4746e4568808c36257d17e7b0db4b04ee
Brajmohank1 <brajmohanst9661@gmail.com>
2024-07-16 12:18:19 +0530

    backend/feat/ticket-booking-list

--- Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/TicketBookingExtra.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/TicketBookingExtra.hs
@@ -1,11 +1,16 @@
 module Storage.Queries.TicketBookingExtra where
 
+import qualified Database.Beam as B
+import qualified Domain.Types.MerchantOperatingCity
 import qualified Domain.Types.TicketBooking as DTB
+import qualified Domain.Types.TicketPlace as DTP
+import qualified EulerHS.Language as L
 import Kernel.Beam.Functions
 import Kernel.Prelude
 import Kernel.Types.Id as Id
 import Kernel.Utils.Common (CacheFlow, EsqDBFlow, MonadFlow)
 import Sequelize as Se
+import qualified Storage.Beam.Common as BeamCommon
 import Storage.Beam.TicketBooking as BeamR
 import Storage.Queries.OrphanInstances.TicketBooking ()
 
@@ -16,3 +21,39 @@ findByIds ::
   m [DTB.TicketBooking]
 findByIds ticketBookingIds = do
   findAllWithKV [Se.Is BeamR.id $ Se.In $ getId <$> ticketBookingIds]
+
+findAllTicketBookingList ::
+  (EsqDBFlow m r, MonadFlow m, CacheFlow m r) =>
+  Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity ->
+  Maybe Int ->
+  Maybe Int ->
+  Maybe UTCTime ->
+  Maybe UTCTime ->
+  Id DTP.TicketPlace ->
+  Maybe Text ->
+  Maybe DTB.BookingStatus ->
+  m [DTB.TicketBooking]
+findAllTicketBookingList merchantOperatingCityId mbLimit mbOffset mbFrom mbTo ticketPlaceId mbTicketBookingShortId mbTicketBookingStatus = do
+  let limitVal = fromMaybe 5 mbLimit
+      offsetVal = fromMaybe 0 mbOffset
+  dbConf <- getMasterBeamConfig
+  res <-
+    L.runDB dbConf $
+      L.findRows $
+        B.select $
+          B.limit_ (fromIntegral limitVal) $
+            B.offset_ (fromIntegral offsetVal) $
+              B.filter_'
+                ( \ticketBooking ->
+                    ( maybe (B.sqlBool_ $ B.val_ True) (\ticketBookingStatus -> ticketBooking.status B.==?. B.val_ ticketBookingStatus) mbTicketBookingStatus
+                    )
+                      B.&&?. (ticketBooking.merchantOperatingCityId B.==?. B.val_ (getId merchantOperatingCityId))
+                      B.&&?. (ticketBooking.ticketPlaceId B.==?. B.val_ (getId ticketPlaceId))
+                      B.&&?. maybe (B.sqlBool_ $ B.val_ True) (\ticketBookingShortId -> ticketBooking.shortId B.==?. B.val_ ticketBookingShortId) mbTicketBookingShortId
+                      B.&&?. maybe (B.sqlBool_ $ B.val_ True) (\defaultFrom -> B.sqlBool_ $ ticketBooking.createdAt B.>=. B.val_ defaultFrom) mbFrom
+                      B.&&?. maybe (B.sqlBool_ $ B.val_ True) (\defaultTo -> B.sqlBool_ $ ticketBooking.createdAt B.<=. B.val_ defaultTo) mbTo
+                )
+                (B.all_ (BeamCommon.ticketBooking BeamCommon.atlasDB))
+  case res of
+    Right res' -> catMaybes <$> mapM fromTType' res'
+    Left _ -> pure []
--- Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/TicketBookingServiceExtra.hs
+++ Backend/app/rider-platform/rider-app/Main/src/Storage/Queries/TicketBookingServiceExtra.hs
@@ -1,14 +1,18 @@
 module Storage.Queries.TicketBookingServiceExtra where
 
 import qualified Data.Time.Calendar
+import qualified Database.Beam as B
 import qualified Domain.Types.BusinessHour as DTBH
+import qualified Domain.Types.MerchantOperatingCity
 import qualified Domain.Types.TicketBookingService as DTBS
 import qualified Domain.Types.TicketService as DTS
+import qualified EulerHS.Language as L
 import Kernel.Beam.Functions
 import Kernel.Prelude
 import qualified Kernel.Types.Id as Id
 import Kernel.Utils.Common (CacheFlow, EsqDBFlow, MonadFlow)
 import Sequelize as Se
+import qualified Storage.Beam.Common as BeamCommon
 import qualified Storage.Beam.TicketBookingService as BeamR
 import Storage.Queries.OrphanInstances.TicketBookingService ()
 
@@ -41,3 +45,39 @@ findByVisitDateAndStatusAndServiceIdAndBtype visitDate status (Id.Id ticketServi
           Se.Is BeamR.expiryDate $ Se.GreaterThanOrEq expiryDate
         ]
     ]
+
+findAllTicketBookingServiceList ::
+  (EsqDBFlow m r, MonadFlow m, CacheFlow m r) =>
+  Id.Id Domain.Types.MerchantOperatingCity.MerchantOperatingCity ->
+  Maybe Int ->
+  Maybe Int ->
+  Maybe UTCTime ->
+  Maybe UTCTime ->
+  Id.Id DTBS.TicketBookingService ->
+  Maybe Text ->
+  Maybe DTBS.ServiceStatus ->
+  m [DTBS.TicketBookingService]
+findAllTicketBookingServiceList merchantOperatingCityId mbLimit mbOffset mbFrom mbTo ticketServiceId mbShortId mbStatus = do
+  let limitVal = fromMaybe 5 mbLimit
+      offsetVal = fromMaybe 0 mbOffset
+  dbConf <- getMasterBeamConfig
+  res <-
+    L.runDB dbConf $
+      L.findRows $
+        B.select $
+          B.limit_ (fromIntegral limitVal) $
+            B.offset_ (fromIntegral offsetVal) $
+              B.filter_'
+                ( \tickekBookingService ->
+                    ( maybe (B.sqlBool_ $ B.val_ True) (\status -> tickekBookingService.status B.==?. B.val_ status) mbStatus
+                    )
+                      B.&&?. (tickekBookingService.merchantOperatingCityId B.==?. B.val_ (Id.getId merchantOperatingCityId))
+                      B.&&?. (tickekBookingService.ticketServiceId B.==?. B.val_ (Id.getId ticketServiceId))
+                      B.&&?. maybe (B.sqlBool_ $ B.val_ True) (\ticketBookingShortId -> tickekBookingService.shortId B.==?. B.val_ ticketBookingShortId) mbShortId
+                      B.&&?. maybe (B.sqlBool_ $ B.val_ True) (\defaultFrom -> B.sqlBool_ $ tickekBookingService.createdAt B.>=. B.val_ defaultFrom) mbFrom
+                      B.&&?. maybe (B.sqlBool_ $ B.val_ True) (\defaultTo -> B.sqlBool_ $ tickekBookingService.createdAt B.<=. B.val_ defaultTo) mbTo
+                )
+                (B.all_ (BeamCommon.tickekBookingService BeamCommon.atlasDB))
+  case res of
+    Right res' -> catMaybes <$> mapM fromTType' res'
+    Left _ -> pure []
